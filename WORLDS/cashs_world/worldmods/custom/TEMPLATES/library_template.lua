e142cccf63bcec4d261be3a0d9c0255b081647aa
rfluis <rfranciscoluis@gmail.com>
2018-08-30 16:33:44 +0200

    master

--- WORLDS/cashs_world/worldmods/custom/TEMPLATES/library_template.lua
+++
@@ -1,8 +0,0 @@
--- library files must end in '_library.lua'
-
--- scan for files that have filename endings that relate to what the item is. Such as:
--- '_tool', '_mob', '_node', '_ore', etc.
-
--- Get each item's definitions from its respective file
--- Register each item
-
--- WORLDS/cashs_world/worldmods/custom/libs/tools_library.lua
+++
@@ -1,3 +0,0 @@
---
--- Scan for files that end in '_tools.lua'
--- Register each tool
