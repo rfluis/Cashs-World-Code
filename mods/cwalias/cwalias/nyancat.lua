7546be67d748303b652995ac751a5a56a5526aa6
rfluis <rfranciscoluis@gmail.com>
2018-08-30 16:08:24 +0200

    cwalias mod

---
+++ mods/cwalias/cwalias/nyancat.lua
@@ -0,0 +1,3 @@
+minetest.register_alias("custom:nyancat_rainbow","nyancat:nyancat_rainbow")
+minetest.register_alias("custom:nyancat","nyancat:nyancat")
+
---
+++ mods/cwalias/cwalias/vessels.lua
@@ -0,0 +1,5 @@
+minetest.register_alias("custom:glass_fragments","vessels:glass_fragments")
+minetest.register_alias("custom:vessels_shelf","vessels:shelf")
+minetest.register_alias("custom:steel_bottle","vessels:steel_bottle")
+minetest.register_alias("custom:glass_bottle","vessels:glass_bottle")
+minetest.register_alias("custom:drinking_glass","vessels:drinking_glass")
